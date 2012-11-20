7f5a96237ba5a791bdc4c705dd614bccf556f969
James Abley <james.abley@gmail.com>
2012-11-20 20:35:59 +0000

    Exercise 8 - Add uncurry

--- exercises/src/main/scala/fpinscala/structuringprograms/Exercises.scala
+++ exercises/src/main/scala/fpinscala/structuringprograms/Exercises.scala
@@ -32,7 +32,8 @@ object Exercises {
   def curry[A,B,C](f: (A, B) => C): A => B => C =
     a => b => f(a, b)
 
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C = sys.error("todo")
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
 
   def compose[A,B,C](f: B => C, g: A => B): A => C = sys.error("todo")
 

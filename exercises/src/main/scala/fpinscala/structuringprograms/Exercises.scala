0c0deab370bcd430ac14feb6f2d56b295618bb87
James Abley <james.abley@gmail.com>
2012-11-21 21:22:10 +0000

    Define lift3 in terms of lift

--- exercises/src/main/scala/fpinscala/structuringprograms/Exercises.scala
+++ exercises/src/main/scala/fpinscala/structuringprograms/Exercises.scala
@@ -39,7 +39,7 @@ object Exercises {
   def lift3[A,B,C,D,E](f: (B, C, D) => E)(g: A => B,
                                           h: A => C,
                                           i: A => D): A => E =
-    a => f(g(a), h(a), i(a))
+    a => lift[A,C,D,E](f(g(a), _, _))(h, i)(a)
 
   def fib(n: Int): Int = sys.error("todo")
 
